d616446b5049ff0a8fdcd5f7f7b3b0519bfc194f
agarmir0920 <tsarevantuan@yandex.ru>
2022-04-18 14:55:05 +0300

    Written hex input module

--- LAB_06/input_hex.asm
+++ LAB_06/input_hex.asm
@@ -1,16 +1,82 @@
-inputCode segment para public 'code'
-    
-    mov ax, msgs
-    mov ds, ax
-    mov dx, offset inputMsg
+public hexInput
 
-    mov ah, 09h
-    int 21h
+numData segment para common 'data'
+    num label word
+numData ends
 
-    mov ah, 0ah
-    mov dx, offset strBuf
+buf segment para public 'data'
+    strBuf db 5
+    strLen db 0
+    text db 5 dup (0)
+buf ends
 
-    int 21h
+inputMsg segment para public 'data'
+    msg db 13, 10
+        db "Input number: "
+        db "$"
+inputMsg ends
 
-inputCode ends
+code segment para public 'code'
+    hexInput proc near
+        assume ds:inputMsg
 
+        mov ax, inputMsg
+        mov ds, ax
+        mov dx, offset msg
+        mov ah, 09h
+        int 21h
+
+        assume ds:buf, es:numData
+
+        mov ax, buf
+        mov ds, ax
+
+        mov ax, numData
+        mov es, ax
+
+        mov dx, offset strBuf
+        mov ah, 0ah
+        int 21h
+
+        cmp strLen, 01h
+        jb exitHexInput
+
+        mov num, 0000h
+        mov cx, 0000h
+
+        parseLoop:
+        cmp cl, strLen
+        jae exitHexInput
+
+        mov si, cx
+
+        mov bl, [text + si]
+        mov bh, 00h
+
+        cmp bl, 41h
+        jae convertLetter
+        jb convertDigit
+
+        convertLetter:
+        sub bl, 37h
+        jmp appendDigit
+
+        convertDigit:
+        sub bl, 30h
+
+        appendDigit:
+        mov ax, 0010h
+        mul num
+        mov num, ax
+        add num, bx
+
+        inc cx
+
+        jmp parseLoop
+
+        exitHexInput:
+        ret
+    hexInput endp
+code ends
+
+end
--- LAB_06/main.asm
+++ LAB_06/main.asm
@@ -1,74 +1,55 @@
 extrn hexInput: near
-extrn binOutput: near
-extrn octOutput: near
+; extrn binOutput: near
+; extrn octOutput: near
 
 stck segment para stack 'stack'
-
     db 100h dup (0)
-
 stck ends
 
 funcPtrs segment para public 'data'
-
     funcPtrsArr dw 3 dup (0)
-
 funcPtrs ends
 
 menu segment para public 'data'
-
     menuText db 13, 10
-             db "1. Input 16-bit unsigned hex number"
              db 13, 10
-             db "2. Print 16-bit unsigned bin number"
+             db "0. Input 16-bit unsigned hex number"
              db 13, 10
-             db "3. Print 16-bit signed oct number"
+             db "1. Print 16-bit unsigned bin number"
              db 13, 10
-             db 13, 10 
-             db "0. Quit"
-             db 13, 10
-             db 13, 10
+             db "2. Print 16-bit signed oct number"
+             db 13, 10, 13, 10
+             db "3. Quit"
+             db 13, 10, 13, 10
              db "Input command: "
              db "$"
-
-    ; inputMsg db 13, 10
-    ;          db "Input number: "
-    ;          db "$"
 
     ; resMsg   db 13, 10
     ;          db "Result: "
     ;          db "$"
-
 menu ends
 
-numData segment para public 'data'
-
-    num dw 0
-
+numData segment para common 'data'
+    num dw 0000h
 numData ends
 
 code segment para public 'code'
-
-    ; заполняем массив указателей на подпрограммы
-
+    main:
     assume es:funcPtrs, cs:code
 
     mov ax, funcPtrs
     mov es, ax
 
-    mov ax, offset hexInput
-    mov [funcPtrsArr], ax
-
-    mov ax, offset binOutput
-    mov [funcPtrsArr + 1], ax
-
-    mov ax, offset octOutput
-    mov [funcPtrsArr + 2], ax
-
-    ; цикл работы программы
-
-    assume ds:menu
+    mov [funcPtrsArr], hexInput
+    ; mov [funcPtrsArr + 2], binOutput
+    ; mov [funcPtrsArr + 4], octOutput
 
     mainLoop:
+    assume ds:menu, es:funcPtrs
+
+    mov ax, funcPtrs
+    mov es, ax
+
     mov ax, menu
     mov ds, ax
 
@@ -76,10 +57,10 @@ code segment para public 'code'
     mov dx, offset menuText
     int 21h
 
-    ; ввод команды
     mov ah, 01h
     int 21h
 
+    mov bh, 00h
     mov bl, al
 
     mov ah, 02h
@@ -88,37 +69,28 @@ code segment para public 'code'
     mov dl, 0ah
     int 21h
 
-    ; выполнение команды
     cmp bl, 30h
-    je exit
-
-    cmp bl, 31h
-    je callHexInput
-
-    cmp bl, 32h
-    je callBinOutput
+    jb mainLoop
 
     cmp bl, 33h
-    je callOctOutput
+    ja mainLoop
 
-    jmp mainLoop
+    sub bl, 30h
 
-    callHexInput:
-    call [funcPtrsArr]
-    jmp mainLoop
+    cmp bl, 03h
+    je exit
 
-    callBinOutput:
-    call [funcPtrsArr + 1]
-    jmp mainLoop
+    mov al, 02h
+    mul bl
+    mov si, ax
 
-    callOctOutput:
-    call [funcPtrsArr + 2]
+    call [funcPtrsArr + si]
+
     jmp mainLoop
 
     exit:
     mov ax, 4c00h
     int 21h
-
 code ends
 
-end mainLoop
+end main
